9f730b085fb7da40b81095a076291fb3145dcb96
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-06-08 22:16:46 +0800

    added home page component content

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,22 @@ const Home = () => {
   return ( 
     <div className="portfolio">
         <h2>Home Page</h2>
+        <br />
+        <h4>Where am I from?</h4>
+        <p>
+        I grew up in Pretoria, South Africa. After studing Bcom(Law) at the University of Pretoria I moved to Johannesburg before moving to the Western Cape, South Coast. In Dec 2017 I relocated to Australia and have been a Perth local since.
+        </p>
+        <br />
+        <h4>Things I like To Focus On</h4>
+        <p>
+        At work I love all things marketing and sales. I enjoy creative thinking and finding solutions. When not at work I love spending time with my family, being outdoors walking, jogging, cycling and kayaking.        </p>
+        <br />
+        <h4>On my radar</h4>
+        <p>
+        I like how websites can be functional and creative at the same time. Finding back end solutions to problems and presenting those solutions to the user with a smart and good-looking front end is an apealing challenge to me.        </p>
+
     </div>
  );
 }
 
- 
 export default Home;
