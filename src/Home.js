b99aa74aae28694aa7198e9d0390b85e435d3a0d
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-06-08 12:23:48 +0800

    added some States to handle errors

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,32 @@ import PortfolioList from "./PortfolioList";
 
 const Home = () => {
   const [portfolios, setPortfolios] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch('http://localhost:8000/portfolios')
+    fetch('http://localhost:8000/portfolioss')
       .then(res => {
+        if(!res.ok) {
+          throw Error('Could not fetch the requested data');
+        }
         return res.json();
       })
       .then(data => {
         setPortfolios(data);
+        setisLoading(false);
+        setError(null);
+      })
+      .catch(err => {
+        setisLoading(false);
+        setError(err.message);
       })
   }, [])
 
   return (
     <div className="home">
+      { error && <div> {error}</div> } 
+      { isLoading && <div>Loading...</div> }
       {portfolios && <PortfolioList portfolios={portfolios} title="All Portfolios" />}
       {portfolios && <PortfolioList portfolios={portfolios.filter((portfolio) => portfolio.deploy === true)} title="Deployed Portfolios" />}
     </div>
