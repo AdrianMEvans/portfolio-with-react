fe14d3ba1123150eadee6f4cd9c52583079dff97
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-06-08 13:16:19 +0800

    created custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,13 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "./PortfolioList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [portfolios, setPortfolios] = useState(null);
-  const [isLoading, setisLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/portfolios')
-      .then(res => {
-        if(!res.ok) {
-          throw Error('Could not fetch the requested data');
-        }
-        return res.json();
-      })
-      .then(data => {
-        setPortfolios(data);
-        setisLoading(false);
-        setError(null);
-      })
-      .catch(err => {
-        setisLoading(false);
-        setError(err.message);
-      })
-  }, [])
+  const {  data: portfolios, isLoading, error } = useFetch('http://localhost:8000/portfolios');
 
   return (
     <div className="home">
-      { error && <div> {error}</div> } 
+      { error && <div>{ error }</div> }      
       { isLoading && <div>Loading...</div> }
       {portfolios && <PortfolioList portfolios={portfolios} title="All Portfolios" />}
       {portfolios && <PortfolioList portfolios={portfolios.filter((portfolio) => portfolio.deploy === true)} title="Deployed Portfolios" />}
