76e7cf27e6741599dc04cc03f85a1b154592ff30
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-06-08 20:17:06 +0800

    added back to list button

--- src/PortfolioDetail.js
+++ src/PortfolioDetail.js
@@ -5,51 +5,62 @@ import ButtonGroup from '@material-ui/core/ButtonGroup';
 import Button from '@material-ui/core/Button';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import HttpIcon from '@material-ui/icons/Http';
-import '@fontsource/roboto';
+import ViewListIcon from '@material-ui/icons/ViewList';import '@fontsource/roboto';
 
 const PortfolioDetail = () => {
     const { id } = useParams();
     const { data: portfolios, error, isLoading } = useFetch('http://localhost:8000/portfolios/' + id);
 
-    return ( 
-        
+    return (
+
         <div className="portfolio-detail">
-            { isLoading && <div>Loading...</div> }
-      { error && <div>{ error }</div> }
-      { portfolios && (
-        <article>
-          <h2>{ portfolios.title }</h2>
-          <img src={ portfolios.image } alt=""/>
-          <h4>Developed by { portfolios.author }</h4>
-          <div>{ portfolios.body }</div>
-          <ButtonGroup variant="contained">
-                <Button
-                  startIcon={<GitHubIcon />}
-                  href={portfolios.gitHub}
-                  style={{
-                    fontSize: 10
-                  }}
-                  target="_blank"
-                  variant="contained"
-                  color="secondary">
-                  Project Repo
+            { isLoading && <div>Loading...</div>}
+            { error && <div>{error}</div>}
+            { portfolios && (
+                <article>
+                    <h2>{portfolios.title}</h2>
+                    <img src={portfolios.image} alt="" />
+                    <h4>Developed by {portfolios.author}</h4>
+                    <div>{portfolios.body}</div>
+                    <ButtonGroup variant="contained">
+                        <Button
+                            startIcon={<GitHubIcon />}
+                            href={portfolios.gitHub}
+                            style={{
+                                fontSize: 10
+                            }}
+                            target="_blank"
+                            variant="contained"
+                            color="secondary">
+                            Project Repo
         </Button>
-                <Button
-                  startIcon={<HttpIcon />}
-                  href={portfolios.deployLink}
-                  style={{
-                    fontSize: 10
-                  }}
-                  target="_blank"
-                  variant="contained"
-                  color="secondary">
-                  Published Project
+                        <Button
+                            startIcon={<HttpIcon />}
+                            href={portfolios.deployLink}
+                            style={{
+                                fontSize: 10
+                            }}
+                            target="_blank"
+                            variant="contained"
+                            color="secondary">
+                            Published Project
         </Button>
-              </ButtonGroup>
-        </article>
-      )}
+                        <Button
+                            startIcon={<ViewListIcon />}
+                            href="/portfolio"
+                            style={{
+                                fontSize: 10
+                            }}
+                            target="_blank"
+                            variant="outlined"
+                            color="secondary">
+                            Back to List
+        </Button>
+                    </ButtonGroup>
+                </article>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default PortfolioDetail;
