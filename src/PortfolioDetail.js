f3b40ae4fbaecd132ee7b139d0380270ec9af885
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-06-08 17:37:20 +0800

    added hook to get id of project for portfolio

--- src/PortfolioDetail.js
+++ src/PortfolioDetail.js
@@ -1,7 +1,10 @@
+import { useParams } from "react-router-dom";
+
 const PortfolioDetail = () => {
+    const { id } = useParams()
     return ( 
         <div className="portfolio-detail">
-            <h2>Portfolio Detail</h2>
+            <h2>Portfolio Detail - { id } </h2>
         </div>
      );
 }
--- src/PortfolioListNotDeployed.js
+++ src/PortfolioListNotDeployed.js
@@ -1,16 +1,20 @@
-const PortfolioList = ({ portfolios, title}) => {
-    
-    return (
-      <div className="portfolio-list">
-        <h2>{ title }</h2>
-        {portfolios.map(portfolio => (
-          <div className="portfolio-preview" key={portfolio.id} >
-            <h2>{ portfolio.title }</h2>
-            <p>Developed by { portfolio.author }</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
- 
+import { Link } from "react-router-dom";
+
+const PortfolioList = ({ portfolios, title }) => {
+
+  return (
+    <div className="portfolio-list">
+      <h2>{title}</h2>
+      {portfolios.map(portfolio => (
+        <div className="portfolio-preview" key={portfolio.id} >
+          <Link to={`/portfolio/${portfolio.id}`}>
+            <h2>{portfolio.title}</h2>
+            <p>Developed by {portfolio.author}</p>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default PortfolioList;
