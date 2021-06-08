bc20d57f8406ce75925d137b61553107c16543b4
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2021-06-08 14:12:11 +0800

    modified Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,32 @@
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>Adrian Evans Profile</h1>
-            <div className="links">
-                <a href="/" href="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>Home</a>
-                <a href="/portfolio" href="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>Portfolio</a>
-                <a href="/contact" href="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>Contact</a>
-            </div>
-        </nav>
-     );
+  return (
+    <nav className="navbar">
+      <h1>Adrian Evans Profile</h1>
+      <div className="links">
+        <a href="/portfolio"
+        //         style={{ 
+        //   color: 'white', 
+        //   backgroundColor: '#f1356d',
+        //   borderRadius: '8px' 
+        // }}
+        >Portfolio</a>
+        <a href="/contact"
+        //         style={{ 
+        //   color: 'white', 
+        //   backgroundColor: '#f1356d',
+        //   borderRadius: '8px' 
+        // }}
+        >Contact</a>
+        <a href="/"
+          style={{
+            color: 'white',
+            backgroundColor: '#f1356d',
+            borderRadius: '8px'
+          }}
+        >Home</a>
+      </div>
+    </nav>
+  );
 }
- 
+
 export default Navbar;
